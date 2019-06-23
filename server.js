1ce4dd136aa27a0384b2806a11e678dfcaa9b665
fhoward123 <42585731+fhoward123@users.noreply.github.com>
2019-06-22 22:06:10 -0500

    Modify server.js to use MongoDB on Heroku

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+
+// Environment Variables
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/forum'
 const PORT = process.env.PORT || 3000
+const secret = process.env.SECRET
+
+//Express listener
+app.listen(PORT, ()=> {
+    console.log(`App listening on port ${PORT}...`);
+});
 
 // Log HTTP requests
 app.use(morgan('tiny'));
@@ -12,11 +21,18 @@ app.use(express.json()); //use .json(), not .urlencoded()
 const bookmarkController = require('./controllers/bookmarks.js');
 app.use('/bookmarks', bookmarkController);
 
-mongoose.connect('mongodb://localhost:27017/meancrud', { useNewUrlParser: true });
-mongoose.connection.once('open', function() {
-    console.log('connected to mongoose...');
-});
+//////////////////////////////////
+//          Mongoose
+/////////////////////////////////
+// Connect to Mongo
+mongoose.connect(mongoURI, { useNewUrlParser: true },
+    () => console.log('MongoDB connection established:', mongoURI)
+);
+// Error / Disconnection
+db.on('error', err => console.log(err.message + ' is Mongod running?'))
+db.on('disconnected', () => console.log('mongo disconnected'))
 
-app.listen(3000, ()=>{
-    console.log('listening...');
-});
+//404 error
+app.get('*', (req, res) => {
+    res.status(404).json('DOH!, page not found')
+})
